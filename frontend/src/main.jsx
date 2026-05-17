7fde2e8d5796053f3615c393e28863bcb10216d0
Swarup Salunke <swarupsalunke01@gmail.com>
2026-05-17 23:26:14 +0530

    fixed css import

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,7 +5,7 @@ import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import store from './store/store'
 import './index.css'
-import './app.css'
+import './App.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
