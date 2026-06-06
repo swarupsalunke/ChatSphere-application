5a17c1a05d945a72cac43da61648bedd734535fe
Swarup Salunke <swarupsalunke01@gmail.com>
2026-06-06 14:42:28 +0530

    Fix error

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,28 @@
+import { useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Chat from "./pages/Chat";
 
 function App() {
+  useEffect(() => {
+    const errorHandler = (e) => {
+      alert("ERROR: " + e.message);
+    };
+
+    const rejectionHandler = (e) => {
+      alert("PROMISE ERROR: " + e.reason);
+    };
+
+    window.addEventListener("error", errorHandler);
+    window.addEventListener("unhandledrejection", rejectionHandler);
+
+    return () => {
+      window.removeEventListener("error", errorHandler);
+      window.removeEventListener("unhandledrejection", rejectionHandler);
+    };
+  }, []);
+
   return (
     <Routes>
       <Route path="/" element={<Login />} />
