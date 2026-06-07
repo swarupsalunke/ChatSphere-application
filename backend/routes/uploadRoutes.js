61791b63257262eafa60a655903e83bd915861f7
Swarup Salunke <swarupsalunke01@gmail.com>
2026-06-07 17:41:34 +0530

    Move uploads to Cloudinary

--- backend/routes/uploadRoutes.js
+++ backend/routes/uploadRoutes.js
@@ -1,27 +1,50 @@
+// const express = require("express");
+// const router = express.Router();
+// const multer = require("multer");
+// const path = require("path");
+
+// // storage config
+// const storage = multer.diskStorage({
+//   destination: "uploads/",
+//   filename: (req, file, cb) => {
+//     cb(null, Date.now() + path.extname(file.originalname));
+//   },
+// });
+
+// const upload = multer({ storage });
+
+// // upload route
+// router.post("/", upload.single("file"), (req, res) => {
+//   const baseUrl =
+//     process.env.NODE_ENV === "production"
+//       ? "https://chatsphere-application-2.onrender.com"
+//       : "http://localhost:5000";
+
+//   res.json({
+//     fileUrl: `${baseUrl}/uploads/${req.file.filename}`,
+//   });
+// });
+
+// module.exports = router;
 const express = require("express");
 const router = express.Router();
 const multer = require("multer");
-const path = require("path");
+const cloudinary = require("../config/cloudinary");
+const { CloudinaryStorage } = require("multer-storage-cloudinary");
 
-// storage config
-const storage = multer.diskStorage({
-  destination: "uploads/",
-  filename: (req, file, cb) => {
-    cb(null, Date.now() + path.extname(file.originalname));
+const storage = new CloudinaryStorage({
+  cloudinary,
+  params: {
+    folder: "chatsphere",
+    resource_type: "auto",
   },
 });
 
 const upload = multer({ storage });
 
-// upload route
 router.post("/", upload.single("file"), (req, res) => {
-  const baseUrl =
-    process.env.NODE_ENV === "production"
-      ? "https://chatsphere-application-2.onrender.com"
-      : "http://localhost:5000";
-
   res.json({
-    fileUrl: `${baseUrl}/uploads/${req.file.filename}`,
+    fileUrl: req.file.path,
   });
 });
 
