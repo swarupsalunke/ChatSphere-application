1b030b0352eb12f0980cd2078e0c902311a4e42b
Swarup Salunke <swarupsalunke01@gmail.com>
2026-06-07 17:52:01 +0530

    Move uploads to Cloudinary

--- backend/routes/uploadRoutes.js
+++ backend/routes/uploadRoutes.js
@@ -1,33 +1,5 @@
-// const express = require("express");
-// const router = express.Router();
-// const multer = require("multer");
-// const path = require("path");
 
-// // storage config
-// const storage = multer.diskStorage({
-//   destination: "uploads/",
-//   filename: (req, file, cb) => {
-//     cb(null, Date.now() + path.extname(file.originalname));
-//   },
-// });
-
-// const upload = multer({ storage });
-
-// // upload route
-// router.post("/", upload.single("file"), (req, res) => {
-//   const baseUrl =
-//     process.env.NODE_ENV === "production"
-//       ? "https://chatsphere-application-2.onrender.com"
-//       : "http://localhost:5000";
-
-//   res.json({
-//     fileUrl: `${baseUrl}/uploads/${req.file.filename}`,
-//   });
-// });
-
-// module.exports = router;
-const express = require("express");
-const router = express.Router();
+const express = require("express");const router = express.Router();
 const multer = require("multer");
 const cloudinary = require("../config/cloudinary");
 const { CloudinaryStorage } = require("multer-storage-cloudinary");
