ecff9d2468b644729d118184394c7d5daba47cb7
Swarup Salunke <swarupsalunke01@gmail.com>
2026-06-10 12:31:55 +0530

    fix error

--- backend/config/firebaseAdmin.js
+++ backend/config/firebaseAdmin.js
@@ -1,7 +1,11 @@
 const { initializeApp, cert } = require("firebase-admin/app");
 const { getMessaging } = require("firebase-admin/messaging");
 
-const serviceAccount = require("./firebase-admin.json");
+const serviceAccount = {
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+  privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+};
 
 const app = initializeApp({
   credential: cert(serviceAccount),
