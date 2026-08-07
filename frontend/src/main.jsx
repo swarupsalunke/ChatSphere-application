084b5631fc04132768d8335467eeeafa353f5eed
Swarup Salunke <swarupsalunke01@gmail.com>
2026-08-07 13:57:06 +0530

    Added PWA support to ChatSphere

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,19 +1,25 @@
 window.global = window;
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import { Provider } from 'react-redux'
-import store from './store/store'
-import './index.css'
-import './App.css'
-import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
+import { registerSW } from "virtual:pwa-register";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./store/store";
+import "./index.css";
+import "./App.css";
+import App from "./App.jsx";
+
+registerSW({
+  immediate: true,
+});
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={store}>
       <BrowserRouter>
         <App />
       </BrowserRouter>
     </Provider>
-  </StrictMode>,
-)
+  </StrictMode>
+);
